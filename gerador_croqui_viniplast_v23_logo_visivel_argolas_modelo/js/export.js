2a01cd764e747a119ac1241b88f86f1962352bf3
cleber <cleberlourenco1986@gmail.com>
2026-07-28 10:36:09 -0300

    COTA5

--- gerador_croqui_viniplast_v23_logo_visivel_argolas_modelo/js/export.js
+++ gerador_croqui_viniplast_v23_logo_visivel_argolas_modelo/js/export.js
@@ -1,7 +1,7 @@
 function exportCleanClone(svgEl){
   const clone=svgEl.cloneNode(true);
   // remover guias, box selecionada e cotas opcionais para exportação limpa
-  clone.querySelectorAll(".guide,.selectedBox,.cota-topwidth,.cota-fh").forEach(n=>n.remove());
+  clone.querySelectorAll(".guide,.selectedBox,.cota-topwidth").forEach(n=>n.remove());
   return clone;
 }
 
